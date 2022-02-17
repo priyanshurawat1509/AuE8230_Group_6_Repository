e2adf100c352d6fa7b55760a7376d2e6c0993b60
Priyanshu Rawat <83747696+priyanshurawat1509@users.noreply.github.com>
2022-02-17 09:14:35 -0500

    Update emergency_braking.py

--- assignment3_turtlebot3/src/scripts/emergency_braking.py
+++ assignment3_turtlebot3/src/scripts/emergency_braking.py
@@ -18,14 +18,10 @@ def callback(msg):
         velocity_publisher.publish(vel_msg)
         range = msg.ranges 
         
-        
         if range[0] < 2:
             stop()
         print(range)
-        move()
-
         
-
 def stop():
     vel_msg.linear.x= 0
     velocity_publisher.publish(vel_msg)
@@ -34,7 +30,6 @@ def main():
     rospy.init_node('emergency_brake', anonymous=True)
     laser_subscriber = rospy.Subscriber('/scan', LaserScan, callback)
     rospy.spin()
-
 
 if __name__ == '__main__':
     try:
